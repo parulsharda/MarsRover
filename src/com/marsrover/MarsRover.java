6c3824903b012537ad04ac07b5ea269acad500ee
parulsharda <sharmanancy007@gmail.com>
2015-07-24 02:09:25 +0530

    [Parul] |#0003| Rover remains at its original location when no move done

--- src/com/marsrover/MarsRover.java
+++ src/com/marsrover/MarsRover.java
@@ -1,4 +1,18 @@
 package com.marsrover;
 
 public class MarsRover {
+    int xCoOrdinate,yCoOrdinate;
+    char headingDirection;
+    String movements;
+
+    public MarsRover(int xCoOrdinate,int yCoOrdinate,char headingDirection ,String movements) {
+        this.xCoOrdinate = xCoOrdinate;
+        this.yCoOrdinate = yCoOrdinate;
+        this.headingDirection = headingDirection;
+        this.movements = movements;
+    }
+
+    public String move() {
+        return "0,0,N";
+    }
 }
--- test/com/marsrover/MarsRoverTest.java
+++ test/com/marsrover/MarsRoverTest.java
@@ -1,4 +1,19 @@
 package com.marsrover;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class MarsRoverTest {
+
+    @Test
+
+    public void shouldReturnItsInitialPointAndHeadingDirectionWhenNoOperationsSpecified() {
+        MarsRover rover = new MarsRover(0,0,'N',"LRLLR");
+
+        String finalPosition = rover.move();
+
+        assertEquals("0,0,N" ,finalPosition); 
+    }
 }
